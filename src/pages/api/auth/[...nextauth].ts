6c1cc334783c3b1c6edb24f926d54d8fe7ad6860
Millena Bomtempo <millena.bomtempo@gmail.com>
2021-12-15 15:21:36 -0300

    verify if user exstis at db, if not, create user

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -17,9 +17,6 @@ export default NextAuth({
       },
     }),
   ],
-  jwt: {
-    secret: process.env.SIGNING_KEY
-  },
   callbacks: {
     async signIn({ user }) {
 
@@ -28,9 +25,25 @@ export default NextAuth({
       try {
 
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email } }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
           )
         )
 
