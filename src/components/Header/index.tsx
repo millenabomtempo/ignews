c122d92c2c50ced3b2c510ea58307655941de638
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-01-18 16:35:12 -0300

    add and config prismic cms

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,22 @@
 import Image from 'next/image';
+import { ActiveLink } from '../ActiveLink';
 import { SignInButton } from '../SignInButton';
 
 import styles from './styles.module.scss';
 
 export function Header () {
+
   return(
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Image src="/images/logo.svg" alt="ig.news" width="110" height="31"/>
         <nav>
+          <ActiveLink activeClassName={styles.active} href="/">
           <a className={styles.active}>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
           <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
