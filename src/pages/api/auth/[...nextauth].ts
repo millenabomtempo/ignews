f3b88ae6d0d4d7b63363a6cb564d70720c3a590f
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-01-20 13:55:18 -0300

    create static page post and preview post

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -18,6 +18,45 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+
+    async session({ session }) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                "active"
+              )]
+            )
+          )
+        )
+  
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        }
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null
+        }
+      }
+      
+    },
+
     async signIn({ user }) {
 
       const { email } = user
@@ -50,9 +89,8 @@ export default NextAuth({
         return true
       } catch {
         return false
-      }
-
-      
+      }      
     }
+
   }
 })
